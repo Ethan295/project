924cf5ac5965dc3fd05778dae5739b31c528b85c
Ethan295 <ethan.indemini@gmail.com>
2023-05-30 08:45:32 +0200

    stufa

---
+++ Stufa.py
@@ -0,0 +1,40 @@
+class Stufa:
+    accesa = False
+
+    def Acecendi(self):
+        self.accesa = True
+
+    def Spegni(self):
+        self.accesa = False
+
+    def Inverso(self):
+        if self.accesa:
+            self.accesa = False
+        else:
+            self.accesa = True
+
+    def toString(self):
+        print("Stufa accesa") if self.accesa else print("Stufa spenta")
+
+
+
+s = Stufa()
+s.toString()
+s.Acecendi()
+s.toString()
+s.Spegni()
+s.toString()
+s.Inverso()
+s.toString()
+s.Inverso()
+s.toString()
+
+
+
+"""
+Fatto meglio
+
+ def inverso(self):
+        self.accesa = not self.accesa
+        
+"""
