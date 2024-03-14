db5b511651f3ebb30f780f7534f780aef51a5488
Ethan295 <ethan.indemini@gmail.com>
2024-03-14 15:46:50 +0100

    a

--- HTML/Giochino/home.js
+++ HTML/Giochino/home.js
@@ -1,11 +1,50 @@
 document.addEventListener('contextmenu', event => event.preventDefault());
 
 
+//definisco ham
+document.addEventListener('DOMContentLoaded', function () {
+
+
+  Object.defineProperty(window, 'ham', {
+    configurable: true, // Consenti la riconfigurazione della proprietà
+    enumerable: true, // Rendi la proprietà enumerabile
+    get: function () {
+      return this._ham; // Utilizza this._ham per evitare conflitti con la funzione ham
+    },
+    set: function (newValue) {
+      this._ham = newValue; // Imposta il valore di _ham
+      onChangeHam(newValue); // Chiama la funzione onChangeHam quando il valore cambia
+    }
+  });
 
 
 
-var ham = 0;
+
+
+  document.getElementById('upgradeButton1').classList.add('disabled');
+  document.getElementById("upgradeButton1").disabled = true;
+
+
+  ham = 0;
+});
+
+
+
 var clickMultiplier = 100;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 function clickPig() {
@@ -29,19 +68,19 @@ function clickPig() {
 
   document.body.appendChild(explosion);
 
-  setTimeout(function() {
-      document.body.removeChild(explosion);
+  setTimeout(function () {
+    document.body.removeChild(explosion);
   }, 520); // Aumentato il tempo di visualizzazione a 1 secondo
 }
 
 function updateHamCounter() {
   var formattedHam;
   if (ham >= 1000) {
-      formattedHam = numeral(ham).format('0,0.00a');
+    formattedHam = numeral(ham).format('0,0.00a');
   } else {
-      formattedHam = ham;
+    formattedHam = ham;
   }
-  document.getElementById("hamCounter").innerHTML = formattedHam + " ham";
+  document.getElementById("hamCounter").innerHTML = formattedHam + " hams";
 }
 
 
@@ -78,4 +117,37 @@ function fetchOtherDataFromDatabase() {
   };
   xhr.send();
 }
+
+
+
+
+function upgrade1() {
+
+
+  console.log("Upgrade 1");
+
+}
+
+
+
+
+
+
+
+function onChangeHam(newValue) {
+  var upg1 = document.getElementById('upgradeButton1').innerHTML;
+
+  if (upg1 < ham) {
+    document.getElementById('upgradeButton1').classList.remove('disabled');
+    document.getElementById("upgradeButton1").disabled = false;
+    upgrade1();
+  } else {
+    document.getElementById('upgradeButton1').classList.add('disabled');
+    document.getElementById("upgradeButton1").disabled = true;
+  }
+
+}
+
+
+
 
