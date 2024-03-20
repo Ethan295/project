f2f8d9686022ea8ed7ac7940f91d3319b694b476
Ethan295 <ethan.indemini@gmail.com>
2024-03-20 11:29:06 +0100

    a

--- HTML/Giochino/home.js
+++ HTML/Giochino/home.js
@@ -1,3 +1,224 @@
+function formatNumber(num) {
+
+
+  if (num >= 1e201) {
+    return (num / 1e201).toFixed(2) + "Millinillion";
+  }
+  if (num >= 1e198) {
+    return (num / 1e198).toFixed(2) + "Nongentillion";
+  }
+  if (num >= 1e195) {
+    return (num / 1e195).toFixed(2) + "Octingentillion";
+  }
+  if (num >= 1e192) {
+    return (num / 1e192).toFixed(2) + "Septingentillion";
+  }
+  if (num >= 1e189) {
+    return (num / 1e189).toFixed(2) + "Sescentillion";
+  }
+  if (num >= 1e186) {
+    return (num / 1e186).toFixed(2) + "Quingentillion";
+  }
+  if (num >= 1e183) {
+    return (num / 1e183).toFixed(2) + "Quadringentillion";
+  }
+  if (num >= 1e180) {
+    return (num / 1e180).toFixed(2) + "Trecentillion";
+  }
+  if (num >= 1e177) {
+    return (num / 1e177).toFixed(2) + "Ducentillion";
+  }
+  if (num >= 1e174) {
+    return (num / 1e174).toFixed(2) + "Nonagintacentillion";
+  }
+  if (num >= 1e171) {
+    return (num / 1e171).toFixed(2) + "Octogintacentillion";
+  }
+  if (num >= 1e168) {
+    return (num / 1e168).toFixed(2) + "Septuagintacentillion";
+  }
+  if (num >= 1e165) {
+    return (num / 1e165).toFixed(2) + "Sexagintacentillion";
+  }
+  if (num >= 1e162) {
+    return (num / 1e162).toFixed(2) + "Quinqua­gintacentillion";
+  }
+  if (num >= 1e159) {
+    return (num / 1e159).toFixed(2) + "Quadragintacentillion";
+  }
+  if (num >= 1e156) {
+    return (num / 1e156).toFixed(2) + "Trigintacentillion";
+  }
+  if (num >= 1e153) {
+    return (num / 1e153).toFixed(2) + "Unviginticentillion";
+  }
+  if (num >= 1e150) {
+    return (num / 1e150).toFixed(2) + "Viginticentillion";
+  }
+  if (num >= 1e147) {
+    return (num / 1e147).toFixed(2) + "Undecicentillion";
+  }
+  if (num >= 1e144) {
+    return (num / 1e144).toFixed(2) + "Decicentillion";
+  }
+  if (num >= 1e141) {
+    return (num / 1e141).toFixed(2) + "Uncentillion";
+  }
+  if (num >= 1e138) {
+    return (num / 1e138).toFixed(2) + "Centillion";
+  }
+  if (num >= 1e135) {
+    return (num / 1e135).toFixed(2) + "Nonagintillion";
+  }
+  if (num >= 1e132) {
+    return (num / 1e132).toFixed(2) + "Octogintillion";
+  }
+  if (num >= 1e129) {
+    return (num / 1e129).toFixed(2) + "Septuagintillion";
+  }
+  if (num >= 1e126) {
+    return (num / 1e126).toFixed(2) + "Sexagintillion";
+  }
+  if (num >= 1e123) {
+    return (num / 1e123).toFixed(2) + "Quinquagintillion";
+  }
+  if (num >= 1e120) {
+    return (num / 1e120).toFixed(2) + "Quadragintillion";
+  }
+  if (num >= 1e117) {
+    return (num / 1e117).toFixed(2) + "Noventrigintillion";
+  }
+  if (num >= 1e114) {
+    return (num / 1e114).toFixed(2) + "Octotrigintillion";
+  }
+  if (num >= 1e111) {
+    return (num / 1e111).toFixed(2) + "Septentrigintillion";
+  }
+  if (num >= 1e108) {
+    return (num / 1e108).toFixed(2) + "Sestrigintillion";
+  }
+  if (num >= 1e105) {
+    return (num / 1e105).toFixed(2) + "Quintrigintillion";
+  }
+  if (num >= 1e102) {
+    return (num / 1e102).toFixed(2) + "Quattuortrigintillion";
+  }
+  if (num >= 1e99) {
+    return (num / 1e99).toFixed(2) + "Trestrigintillion";
+  }
+  if (num >= 1e96) {
+    return (num / 1e96).toFixed(2) + "Duotrigintillion";
+  }
+  if (num >= 1e93) {
+    return (num / 1e93).toFixed(2) + "Untrigintillion";
+  }
+  
+
+  
+
+
+
+
+
+
+
+  if (num >= 1e93) {
+    return (num / 1e93).toFixed(2) + "Tg";
+  }
+  if (num >= 1e90) {
+    return (num / 1e90).toFixed(2) + "Nvg";
+  }
+  if (num >= 1e87) {
+    return (num / 1e87).toFixed(2) + "Ovg";
+  }
+  if (num >= 1e84) {
+    return (num / 1e84).toFixed(2) + "Spv";
+  }
+  if (num >= 1e81) {
+    return (num / 1e81).toFixed(2) + "Svg";
+  }
+  if (num >= 1e78) {
+    return (num / 1e78).toFixed(2) + "Qvg";
+  }
+  if (num >= 1e75) {
+    return (num / 1e75).toFixed(2) + "Qav";
+  }
+  if (num >= 1e72) {
+    return (num / 1e72).toFixed(2) + "Tvg";
+  }
+  if (num >= 1e69) {
+    return (num / 1e69).toFixed(2) + "D";
+  }
+  if (num >= 1e66) {
+    return (num / 1e66).toFixed(2) + "C";
+  }
+  if (num >= 1e63) {
+    return (num / 1e63).toFixed(2) + "V";
+  }
+  if (num >= 1e60) {
+    return (num / 1e60).toFixed(2) + "N";
+  }
+  if (num >= 1e57) {
+    return (num / 1e57).toFixed(2) + "O";
+  }
+  if (num >= 1e54) {
+    return (num / 1e54).toFixed(2) + "St";
+  }
+  if (num >= 1e51) {
+    return (num / 1e51).toFixed(2) + "Sd";
+  }
+  if (num >= 1e48) {
+    return (num / 1e48).toFixed(2) + "Qd";
+  }
+  if (num >= 1e45) {
+    return (num / 1e45).toFixed(2) + "Qt";
+  }
+  if (num >= 1e42) {
+    return (num / 1e42).toFixed(2) + "Tdc";
+  }
+  if (num >= 1e39) {
+    return (num / 1e39).toFixed(2) + "Ddc";
+  }
+  if (num >= 1e36) {
+    return (num / 1e36).toFixed(2) + "Udc";
+  }
+  if (num >= 1e33) {
+    return (num / 1e33).toFixed(2) + "Dc";
+  }
+  if (num >= 1e30) {
+    return (num / 1e30).toFixed(2) + "No";
+  }
+  if (num >= 1e27) {
+    return (num / 1e27).toFixed(2) + "Oc";
+  }
+  if (num >= 1e24) {
+    return (num / 1e24).toFixed(2) + "Sp";
+  }
+  if (num >= 1e21) {
+    return (num / 1e21).toFixed(2) + "Sx";
+  }
+  if (num >= 1e18) {
+    return (num / 1e18).toFixed(2) + "Qi";
+  }
+  if (num >= 1e15) {
+    return (num / 1e15).toFixed(2) + "Qa";
+  }
+  if (num >= 1e12) {
+    return (num / 1e12).toFixed(2) + "T";
+  }
+  if (num >= 1e9) {
+    return (num / 1e9).toFixed(2) + "B";
+  }
+  if (num >= 1e6) {
+    return (num / 1e6).toFixed(2) + "M";
+  }
+  if (num >= 1e3) {
+    return (num / 1e3).toFixed(2) + "K";
+  }
+  return num.toString();
+}
+
+
 
 
 
@@ -68,25 +289,14 @@ function clickPig() {
 }
 
 function updateHamCounter() {
-  var formattedHam;
-  if (ham >= 1000) {
-    formattedHam = numeral(ham).format('0,0.00a');
-  } else {
-    formattedHam = ham;
-  }
-  $("#hamCounter").html(formattedHam + " hams");
+
+  $("#hamCounter").html(formatNumber(ham) + " hams");
 }
 
 
 
 function updateClickMultiplier() {
-  var formattedClickMultiplier;
-  if (clickMultiplier >= 1000) {
-    formattedClickMultiplier = numeral(clickMultiplier).format('0,0.00a');
-  } else {
-    formattedClickMultiplier = clickMultiplier;
-  }
-  $("#clickPowerDisplay").html("Click power: " + formattedClickMultiplier);
+  $("#clickPowerDisplay").html("Click power: " + formatNumber(clickMultiplier));
 }
 
 
@@ -136,14 +346,8 @@ function upgrade1() {
   updateClickMultiplier();
   upg1Price = Math.round(upg1Price * 1.5); // Aggiorna il prezzo dell'aggiornamento
 
-  var valoreFormattato;
-  if (upg1Price >= 1000) {
-    valoreFormattato = numeral(upg1Price).format('0,0.00a');
-  } else {
-    valoreFormattato = upg1Price;
-  }
-
-  $("#upgradeButton1").html(valoreFormattato);
+  console.log(formatNumber(upg1Price));
+  $("#upgradeButton1").html(formatNumber(upg1Price));
 }
 
 
